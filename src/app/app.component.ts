b6b2d40f3c21e43f06b0454fd073a33d99a93978
nadsonsousa95 <nadsonbcs95@gmail.com>
2024-11-22 22:48:26 -0300

    Adição do arquivo json e configurações de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, ReactiveFormsModule]
 })
 export class AppComponent {
   title = 'video-platform';
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,27 @@ import { provideRouter } from '@angular/router'; // Roteamento
 import { LoginComponent } from './app/security/login/login.component';
 import { LayoutComponent } from './app/pages/layout/layout.component';
 import { HomeComponent } from './app/pages/home/home.component';
-
+import { ToastrModule } from 'ngx-toastr';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      ToastrModule.forRoot({
+        timeOut: 3000,
+        positionClass: 'toast-top-right',
+        preventDuplicates: true,
+      })
+    ),
     provideRouter([
       {path: 'login', component: LoginComponent},
       {
           path: '', component: LayoutComponent,
           children:[
-              {path: '', component: HomeComponent }
+              {path: '', component: HomeComponent },
+              //{path: 'usuarios', component: HomeComponent }
           ]
           
       },
